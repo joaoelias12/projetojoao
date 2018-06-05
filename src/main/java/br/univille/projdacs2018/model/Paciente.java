f3e9a32bd4831df64c9b1cb4a4bb3b00d6531fdd
JOÃO MARCOS ELIAS MATIAS <joaomatias@univille.br>
2018-06-04 21:51:38 -0300

    first commit

---
+++ src/main/java/br/univille/projdacs2018/model/Paciente.java
@@ -0,0 +1,27 @@
+package br.univille.projdacs2018.model;
+
+public class Paciente {
+	private long id;
+	private String nome;
+	private String sexo;
+
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
